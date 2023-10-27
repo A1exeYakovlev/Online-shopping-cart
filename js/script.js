3fa410a6eef9ffa25c8921200909197c462b7645
A1exeYakovlev <118552675+A1exeYakovlev@users.noreply.github.com>
2023-10-27 22:01:45 +0300

    Feat: add a link to the delivery interval
    Click on the delivery interval scrolls to the delivery section.

--- js/script.js
+++ js/script.js
@@ -1082,3 +1082,10 @@ cartWrap.addEventListener("click", function (e) {
 
   document.body.addEventListener("click", closeTooltip);
 })
+
+//переход от интервалов доставки в итогах (десктоп) к секции про доставку
+resultDeliveryInterval.addEventListener("click", function (e) {
+  e.preventDefault();
+  const id = e.target.getAttribute("href");
+  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+})
